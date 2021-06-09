473bd82435f8cd5f1243f74f9124a5e9a2a602e6
Administrator <qtw32@naver.com>
2021-06-09 13:31:20 +0900

    add_bus

--- app/src/main/java/com/example/busapplication/BusModel.kt
+++ app/src/main/java/com/example/busapplication/BusModel.kt
@@ -13,8 +13,12 @@ data class BusModel(val routeno: String, val arrprevstationcnt: String, val arrt
         get() {
             return if (arrtime < 0) {
                 "남은 시간: 도착 정보 없음"
-            } else {
-                "남은 시간: ${arrtime / 60}분"
+            }
+            else if (arrtime == 0) {
+                "남은 시간: 곧 도착"
+            }
+            else {
+                "남은 시간: ${arrtime / 60}분 ${arrtime % 60}초"
             }
         }
 }
